2a0e6f16dce69d98cdaecf740ef05c0c6e55673b
LEO <hick317@gmail.com>
2023-11-08 18:28:39 +0530

    add build file

--- backend/app.js
+++ backend/app.js
@@ -19,10 +19,10 @@ app.use("/api/v1/", product);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 
 module.exports = app;
